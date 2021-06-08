d7d977716414340f0bc40b82e4380f457d550fe2
burak.ozcelik <b.ozcelik@baylanwatermeters.com>
2021-06-08 09:47:51 +0300

    Hello World using component of <TextEdit>

--- BlazorDemo/BlazorDemo/obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
+++ BlazorDemo/BlazorDemo/obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
@@ -13,105 +13,105 @@ namespace BlazorDemo
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\BurakOzcelik\repositories\BlazorDemo\BlazorDemo\_Imports.razor"
+#line 1 "C:\Users\BurakOzcelik\repositories\Blazorise\BlazorDemo\BlazorDemo\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\BurakOzcelik\repositories\BlazorDemo\BlazorDemo\_Imports.razor"
+#line 2 "C:\Users\BurakOzcelik\repositories\Blazorise\BlazorDemo\BlazorDemo\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\BurakOzcelik\repositories\BlazorDemo\BlazorDemo\_Imports.razor"
+#line 3 "C:\Users\BurakOzcelik\repositories\Blazorise\BlazorDemo\BlazorDemo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\BurakOzcelik\repositories\BlazorDemo\BlazorDemo\_Imports.razor"
+#line 4 "C:\Users\BurakOzcelik\repositories\Blazorise\BlazorDemo\BlazorDemo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\BurakOzcelik\repositories\BlazorDemo\BlazorDemo\_Imports.razor"
+#line 5 "C:\Users\BurakOzcelik\repositories\Blazorise\BlazorDemo\BlazorDemo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\BurakOzcelik\repositories\BlazorDemo\BlazorDemo\_Imports.razor"
+#line 6 "C:\Users\BurakOzcelik\repositories\Blazorise\BlazorDemo\BlazorDemo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\BurakOzcelik\repositories\BlazorDemo\BlazorDemo\_Imports.razor"
+#line 7 "C:\Users\BurakOzcelik\repositories\Blazorise\BlazorDemo\BlazorDemo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\BurakOzcelik\repositories\BlazorDemo\BlazorDemo\_Imports.razor"
+#line 8 "C:\Users\BurakOzcelik\repositories\Blazorise\BlazorDemo\BlazorDemo\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\BurakOzcelik\repositories\BlazorDemo\BlazorDemo\_Imports.razor"
+#line 9 "C:\Users\BurakOzcelik\repositories\Blazorise\BlazorDemo\BlazorDemo\_Imports.razor"
 using BlazorDemo;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\BurakOzcelik\repositories\BlazorDemo\BlazorDemo\_Imports.razor"
+#line 10 "C:\Users\BurakOzcelik\repositories\Blazorise\BlazorDemo\BlazorDemo\_Imports.razor"
 using BlazorDemo.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\BurakOzcelik\repositories\BlazorDemo\BlazorDemo\_Imports.razor"
+#line 11 "C:\Users\BurakOzcelik\repositories\Blazorise\BlazorDemo\BlazorDemo\_Imports.razor"
 using Blazorise;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\BurakOzcelik\repositories\BlazorDemo\BlazorDemo\_Imports.razor"
+#line 12 "C:\Users\BurakOzcelik\repositories\Blazorise\BlazorDemo\BlazorDemo\_Imports.razor"
 using Blazorise.Bootstrap;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "C:\Users\BurakOzcelik\repositories\BlazorDemo\BlazorDemo\_Imports.razor"
+#line 13 "C:\Users\BurakOzcelik\repositories\Blazorise\BlazorDemo\BlazorDemo\_Imports.razor"
 using Blazorise.DataGrid;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "C:\Users\BurakOzcelik\repositories\BlazorDemo\BlazorDemo\_Imports.razor"
+#line 14 "C:\Users\BurakOzcelik\repositories\Blazorise\BlazorDemo\BlazorDemo\_Imports.razor"
 using Blazorise.DataGrid.Utils;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 15 "C:\Users\BurakOzcelik\repositories\BlazorDemo\BlazorDemo\_Imports.razor"
+#line 15 "C:\Users\BurakOzcelik\repositories\Blazorise\BlazorDemo\BlazorDemo\_Imports.razor"
 using Blazorise.Icons.FontAwesome;
 
 #line default
